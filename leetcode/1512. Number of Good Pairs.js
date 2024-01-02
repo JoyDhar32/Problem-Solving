3f20a013f894732a6109a4af31d8a256325d2775
JoyDhar32 <joyustcian@gmail.com>
2024-01-02 14:26:35 +1100

    accepted

---
+++ leetcode/1512. Number of Good Pairs.js
@@ -0,0 +1,18 @@
+function numIdenticalPairs(nums) {
+    let count = 0;
+    const occurrences = {};
+
+    for (const num of nums) {
+        count += occurrences[num] || 0;
+        occurrences[num] = (occurrences[num] || 0) + 1;
+    }
+
+    return count;
+}
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])); // Output: 4
+console.log(numIdenticalPairs([1, 1, 1, 1])); // Output: 6
+console.log(numIdenticalPairs([1, 2, 3])); // Output: 0
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// ✅
+
