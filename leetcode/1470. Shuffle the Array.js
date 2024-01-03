8f8f9a04162b05700f5e517e40b913d66c3cce76
JoyDhar32 <joyustcian@gmail.com>
2024-01-03 13:26:02 +1100

    accepted

---
+++ leetcode/1470. Shuffle the Array.js
@@ -0,0 +1,14 @@
+const shuffle =(num,n)=>{
+    let result=[];
+    for(let i=0;i<n;i++){
+        result.push(num[i],num[i+n]);
+    }
+    return result;
+}
+console.log(shuffle([2,5,1,3,4,7],3));
+console.log(shuffle([1,2,3,4,4,3,2,1],4));
+console.log(shuffle([1,1,2,2],2));
+// Time Complexity: O(n)
+
+// Space Complexity: O(1)
+// ✅
