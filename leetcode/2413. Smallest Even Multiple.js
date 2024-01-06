8d3fcae2e1d452080898957d1cf091521009ff16
JoyDhar32 <joyustcian@gmail.com>
2024-01-06 23:57:10 +1100

    ✅ Beats 100% of users with JavaScript

---
+++ leetcode/2413. Smallest Even Multiple.js
@@ -0,0 +1,13 @@
+const smallestEvenMultiple =(n)=>{
+if(n%2===0)return n;
+else{
+ return n*2;
+}
+}
+console.log(smallestEvenMultiple(3));
+console.log(smallestEvenMultiple(4));
+console.log(smallestEvenMultiple(5));
+
+// ✅ Beats 100% of users with JavaScript
+
+
