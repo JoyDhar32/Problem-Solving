fe874dd76326d9cc96d4cda844466bb1ff4e885e
JoyDhar32 <joyustcian@gmail.com>
2024-01-07 22:40:45 +1100

    ✅ Beat 55% of solutions on leetcode

---
+++ leetcode/2810. Faulty Keyboard.js
@@ -0,0 +1,19 @@
+const finalString = (s) => {
+    let str = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'i') {
+            str = str.split('').reverse().join('');
+        } else {
+            str += s[i];
+        }
+    }
+
+    return str;
+}
+
+console.log(finalString("abc"));
+console.log(finalString("abciii"));
+console.log(finalString("abciiidef"));
+console.log(finalString("string"));
+console.log(finalString("poiinter"));
