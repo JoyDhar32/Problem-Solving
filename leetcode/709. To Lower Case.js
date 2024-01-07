d45d88be7f6458034af9153def390040b02ca7b6
JoyDhar32 <joyustcian@gmail.com>
2024-01-07 21:10:15 +1100

    ✅ Beats 94.01%of users with JavaScript

---
+++ leetcode/709. To Lower Case.js
@@ -0,0 +1,15 @@
+const toLowerCase =(s)=>{
+    return s.toLowerCase()
+}
+console.log(toLowerCase("Hello"))
+console.log(toLowerCase("here"))
+console.log(toLowerCase("LOVELY"))
+console.log(toLowerCase("al&phaBET"))
+console.log(toLowerCase("123"))
+console.log(toLowerCase("1a2b3c"))
+console.log(toLowerCase("1A2B3C"))
+
+// time complexity: O(n)
+// space complexity: O(n)
+
+// ✅ Beats 94.01%of users with JavaScript
