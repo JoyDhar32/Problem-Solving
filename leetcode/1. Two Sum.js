d7edff8d4709413fca46f8ba8e7d954e79638a9a
JoyDhar32 <joyustcian@gmail.com>
2024-01-08 23:37:13 +1100

    ✅ Beats 95% of JavaScript submissions in runtime

--- leetcode/1. Two Sum.js
+++ leetcode/1. Two Sum.js
@@ -14,3 +14,8 @@ console.log(sum([2,7,11,15],9));
 console.log(sum([3,2,4],6));
 console.log(sum([3,3],6));
 console.log(sum([3,2,3],6));
+
+// time complexity: O(n^2)
+// space complexity: O(1)
+
+// ✅ Beats 95% of JavaScript submissions in runtime
