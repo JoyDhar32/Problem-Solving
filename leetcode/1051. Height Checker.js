a78a94357773f822de2643ff34412cf7f10742f7
JoyDhar32 <joyustcian@gmail.com>
2024-01-13 00:03:19 +1100

    ✅ Hint: sorting array with sallow copy and compare with original array

---
+++ leetcode/1051. Height Checker.js
@@ -0,0 +1,18 @@
+const heightChecker = (heights) => {
+    const sorted = [...heights].sort((a, b) => a - b); // ([...heights]) is used to create a shallow copy of the heights array before sorting it
+    console.log(sorted )
+    let count = 0;
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== sorted[i]) {
+        count++;
+        }
+    }
+    return count;
+    };
+    console.log(heightChecker([1,1,4,2,1,3])); // 3
+    console.log(heightChecker([5,1,2,3,4])); // 5
+    console.log(heightChecker([1,2,3,4,5])); // 0
+    // time complexity: O(n)
+    // space complexity: O(n)
+
+// ✅  Hint: sorting array with sallow copy and compare with original array
