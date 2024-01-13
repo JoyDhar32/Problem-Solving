0f5150f734d430aeb195bf616c723d1b5c131d07
JoyDhar32 <joyustcian@gmail.com>
2024-01-13 22:59:55 +1100

    ✅ Hint: using nested loop

---
+++ leetcode/1748. Sum of Unique Elements.js
@@ -0,0 +1,26 @@
+const sumOfUnique = (nums) => {
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++){
+        let count = 0;
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                count++;
+            }
+        }
+        if(count === 1){
+            sum += nums[i];
+        }
+    }
+    
+    return sum;
+    }
+
+    console.log(sumOfUnique([1,2,3,2])); // 4
+    console.log(sumOfUnique([1,1,1,1,1])); // 0
+    console.log(sumOfUnique([1,2,3,4,5])); // 15
+
+    // time complexity: O(n)
+    // space complexity: O(n)
+
+    //  ✅ Hint: Use a hash map to store the frequency of each number.
+    // Iterate through the array and add the numbers that have a frequency of 1.
