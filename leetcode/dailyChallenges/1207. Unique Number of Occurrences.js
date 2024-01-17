e693b3198b9d39ae3563bdd02a08b844aa5bcb5f
JoyDhar32 <joyustcian@gmail.com>
2024-01-17 15:18:12 +1100

    ✅ Hints: Use an object to count the frequency of each element.

---
+++ leetcode/dailyChallenges/1207. Unique Number of Occurrences.js
@@ -0,0 +1,17 @@
+const uniqueOccurrences=(arr) =>{
+    const frequencyMap = new Map();
+
+    for (const num of arr) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+    }
+
+    const uniqueFrequencies = new Set(frequencyMap.values());
+
+    return uniqueFrequencies.size === frequencyMap.size;  
+}
+
+console.log(uniqueOccurrences([1,2,2,1,1,3])); // Output: true
+console.log(uniqueOccurrences([1,2])); // Output: false
+console.log(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0])); // Output: true
+
+// Hint 1: Use an object to count the frequency of each element.
