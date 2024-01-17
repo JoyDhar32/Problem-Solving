749e505f3327aaf0268f998e8d49e8330b14e188
JoyDhar32 <joyustcian@gmail.com>
2024-01-17 20:01:00 +1100

    ✅ Hints: Just Return False

---
+++ leetcode/medium/2396. Strictly Palindromic Number.js
@@ -0,0 +1,9 @@
+const isStrictlyPalindromic =(n) => {
+    return false;
+}
+ 
+console.log(isStrictlyPalindromic(1)); // false
+console.log(isStrictlyPalindromic(11)); // false
+console.log(isStrictlyPalindromic(121)); // false
+
+// ✅ Hints: Just Return False
