5d6df7ee0cb4413fac762fe615f8271559f013e2
JoyDhar32 <joyustcian@gmail.com>
2024-01-19 13:47:26 +1100

    ✅ Hints: sum of n natural numbers = n*(n+1)/2 - sum of array elements = missing number

---
+++ leetcode/Easy/268. Missing Number.js
@@ -0,0 +1,12 @@
+const missingNumber = (nums)=>{
+    let sum = 0;
+    for(let i=0; i<nums.length; i++){
+        sum += nums[i];
+    }
+    let n = nums.length;
+    return (n*(n+1)/2) - sum;
+}
+console.log(missingNumber([3,0,1])); // 2
+console.log(missingNumber([0,1])); // 2
+console.log(missingNumber([9,6,4,2,3,5,7,0,1])); // 8
+console.log(missingNumber([0])); // 1
