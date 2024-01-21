3e9dace24e8c5cd0779526763b76da2644ed1074
JoyDhar32 <joyustcian@gmail.com>
2024-01-21 22:12:00 +1100

    ✅ Mathematical Deductions

--- leetcode/Easy/1025. Divisor Game.js
+++ leetcode/Easy/1025. Divisor Game.js
@@ -7,7 +7,8 @@ console.log(divisorGame(3)); // false
 console.log(divisorGame(4)); // true
 console.log(divisorGame(5)); // false
 
-// ✅ Mathematical Deduction
+
 // ✅ Time Complexity: O(1)  
 // ✅ Space Complexity: O(1)
 
+// ✅ Mathematical Deduction
