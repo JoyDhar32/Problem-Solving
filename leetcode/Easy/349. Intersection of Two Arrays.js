d857354d9a073ebb4fa39f5761478cead79da89f
JoyDhar32 <joyustcian@gmail.com>
2024-01-26 18:10:19 +1100

    ✅ using set

---
+++ leetcode/Easy/349. Intersection of Two Arrays.js
@@ -0,0 +1,15 @@
+const intersection = (nums1, nums2) => {
+    const set = new Set(nums1);
+    const result = new Set();
+    for (const num of nums2) {
+        if (set.has(num)) result.add(num);
+    }
+    return [...result];
+    };
+console.log(intersection([1,2,2,1], [2,2])); // [2]
+console.log(intersection([4,9,5], [9,4,9,8,4])); // [9,4]
+console.log(intersection([1,2,2,1], [2,2])); // [2]
+
+// time complexity: O(n)
+// space complexity: O(n)
+// ✅ Beats 89.99% of JavaScript submissions in runtime
