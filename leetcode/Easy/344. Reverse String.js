47efbf443bf5db1a10dad2eee93ae55ea3bd99c7
JoyDhar32 <joyustcian@gmail.com>
2024-01-29 17:17:50 +1100

    ✅ Hint: Using reverse function

---
+++ leetcode/Easy/344. Reverse String.js
@@ -0,0 +1,8 @@
+const reverseString =(s)=>{
+    return s.reverse()
+}
+console.log(reverseString(["h","e","l","l","o"]));
+
+// time complexity: O(n)
+// space complexity: O(1)
+// ✅ Hint: Using reverse function
