8d5b7e99ef08b74ac7cc13ab60444fe69f3b33b2
JoyDhar32 <joyustcian@gmail.com>
2024-02-02 12:56:55 +1100

    ✅ Hints:Sequential Digits

--- leetcode/dailyChallenges/1291. Sequential Digits.js
+++ leetcode/dailyChallenges/1291. Sequential Digits.js
@@ -11,6 +11,32 @@ const sequentialDigits =(low, high)=>{
     }
     return result.sort((a,b)=>a-b);
 }
+// Alternative
+
+/*
+var sequentialDigits = function (low, high) {
+    let arr = [
+        12, 23, 34, 45, 56, 67, 78, 89,
+        123, 234, 345, 456, 567, 678, 789,
+        1234, 2345, 3456, 4567, 5678, 6789,
+        12345, 23456, 34567, 45678, 56789,
+        123456, 234567, 345678, 456789,
+        1234567, 2345678, 3456789,
+        12345678, 23456789,
+        123456789
+    ];
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (low <= arr[i] && high >= arr[i]) {
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+*/
+
 console.log(sequentialDigits(100,300)); //[123,234]
 console.log(sequentialDigits(1000,13000)); //[1234,2345,3456,4567,5678,6789,12345]
 console.log(sequentialDigits(10,1000000000)); //[12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789]
+
+//  ✅ Hints:Sequential Digits
