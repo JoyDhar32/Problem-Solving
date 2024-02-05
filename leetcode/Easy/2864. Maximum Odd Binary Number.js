e7429f3d9586b322f06e5cba8490d9b0e3927ea6
JoyDhar32 <joyustcian@gmail.com>
2024-02-05 20:48:12 +1100

    ✅ Hints : Sort the binary string in descending order and then replace the first 0 with 1.

---
+++ leetcode/Easy/2864. Maximum Odd Binary Number.js
@@ -0,0 +1,14 @@
+const maximumOddBinaryNumber =(s)=>{
+let conv=s.split('').sort((a,b)=>(b-a));
+ conv.shift();
+ conv.push('1');
+ return conv.join('');
+    
+}
+console.log(maximumOddBinaryNumber("1101"))//1011
+console.log(maximumOddBinaryNumber("1100"))//1100
+console.log(maximumOddBinaryNumber("110"))//110
+
+// time complexity is O(nlogn)
+// space complexity is O(n)
+// ✅ Hints : Sort the binary string in descending order and then replace the first 0 with 1.
