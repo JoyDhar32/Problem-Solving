64ed67516c349973966625a7bdc1c4c001465b18
JoyDhar32 <joyustcian@gmail.com>
2024-02-10 16:25:26 +1100

    update

---
+++ leetcode/dailyChallenges/647. Palindromic Substrings.js
@@ -0,0 +1,22 @@
+const countSubstrings = (s) =>{
+    let count = 0;
+    for(let i = 0; i < s.length; i++){
+        count += expandAroundCenter(s, i, i);
+        count += expandAroundCenter(s, i, i+1);
+    }
+    return count;
+}
+
+const expandAroundCenter = (s, left, right) =>{
+    let count = 0;
+    while(left >= 0 && right < s.length && s[left] === s[right]){
+        left--;
+        right++;
+        count++;
+    }
+    return count;
+}
+
+console.log(countSubstrings("abc")); // 3
+console.log(countSubstrings("aaa")); // 6
+console.log(countSubstrings("abba")); // 6
