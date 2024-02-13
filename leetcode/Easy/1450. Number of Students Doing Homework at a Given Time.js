ea1a9496bcc30f7a88bbca06b9dc19dde73f14fd
JoyDhar32 <joyustcian@gmail.com>
2024-02-13 20:45:58 +1100

    update

---
+++ leetcode/Easy/1450. Number of Students Doing Homework at a Given Time.js
@@ -0,0 +1,10 @@
+const busyStudent = (startTime, endTime, queryTime) => {
+    let count = 0;
+    for (let i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) count++;
+    }
+    return count;
+    };
+console.log(busyStudent([1, 2, 3], [3, 2, 7], 4));//1
+console.log(busyStudent([4], [4], 4));//1
+console.log(busyStudent([4], [4], 5));//0
