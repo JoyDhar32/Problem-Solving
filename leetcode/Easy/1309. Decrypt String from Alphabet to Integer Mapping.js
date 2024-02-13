f61f608eb9d1598f4936526deb54d3aff246e24e
JoyDhar32 <joyustcian@gmail.com>
2024-02-13 19:56:19 +1100

    ✅ Hints: Using JS Built-in Functions parseInt, fromCharCode

--- leetcode/Easy/1309. Decrypt String from Alphabet to Integer Mapping.js
+++ leetcode/Easy/1309. Decrypt String from Alphabet to Integer Mapping.js
@@ -2,18 +2,18 @@ const freqAlphabets = (s) => {
     let result = '';
     for (let i = 0; i < s.length; i++) {
         if (s[i + 2] === '#') {
-        result += String.fromCharCode(96 + parseInt(s[i] + s[i + 1]));
-        i += 2;
+            result += String.fromCharCode(96 + parseInt(s[i] + s[i + 1]));
+            i += 2;
         } else {
-        result += String.fromCharCode(96 + parseInt(s[i]));
+            result += String.fromCharCode(96 + parseInt(s[i]));
         }
     }
     return result;
-    };
-    console.log(freqAlphabets("10#11#12"));//jkab
-    console.log(freqAlphabets("1326#"));//acz
-    console.log(freqAlphabets("25#"));//y
+};
+console.log(freqAlphabets("10#11#12"));//jkab
+console.log(freqAlphabets("1326#"));//acz
+console.log(freqAlphabets("25#"));//y
 
-    // time complexity is O(n)
-    // space complexity is O(1) because we are not using any extra space
-    // ✅ Hints: Using JS Built-in Functions parseInt, fromCharCode
+// time complexity is O(n)
+// space complexity is O(1) because we are not using any extra space
+// ✅ Hints: Using JS Built-in Functions parseInt, fromCharCode
