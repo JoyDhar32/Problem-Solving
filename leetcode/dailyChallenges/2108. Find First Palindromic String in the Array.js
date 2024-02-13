c4a9b9e739d313cdf60d58ad7b68df10d73f3b6e
JoyDhar32 <joyustcian@gmail.com>
2024-02-13 11:55:04 +1100

    ✅ Hint: Using String

---
+++ leetcode/dailyChallenges/2108. Find First Palindromic String in the Array.js
@@ -0,0 +1,20 @@
+const firstPalindrome =(words) =>{
+    for(let i = 0; i < words.length; i++){
+        let word = words[i];
+        console.log(`word is ${word}`); // Seperate each word
+        let reverseWord = word.split('').reverse().join(''); // Reverse the word and join it back together
+        if(word === reverseWord){
+            return word;
+        }
+    }
+    return "";
+}
+
+console.log(firstPalindrome(["abc","car","ada","racecar","cool"])); // ["a","aba"]
+console.log(firstPalindrome(["notapalindrome","racecar"])); // ["a","aba"]
+
+// time complexity: O(n)
+// space complexity: O(n)
+
+// ✅ Beats 100% of users with JavaScript
+// ✅ Hint: Using String
