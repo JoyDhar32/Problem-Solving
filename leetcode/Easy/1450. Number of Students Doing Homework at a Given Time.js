25dea89c12ad31b6e76087ad3a82d8e30ecc667b
JoyDhar32 <joyustcian@gmail.com>
2024-02-13 20:46:33 +1100

    ✅ Hints: using if condition only

--- leetcode/Easy/1450. Number of Students Doing Homework at a Given Time.js
+++ leetcode/Easy/1450. Number of Students Doing Homework at a Given Time.js
@@ -8,3 +8,7 @@ const busyStudent = (startTime, endTime, queryTime) => {
 console.log(busyStudent([1, 2, 3], [3, 2, 7], 4));//1
 console.log(busyStudent([4], [4], 4));//1
 console.log(busyStudent([4], [4], 5));//0
+
+// time complexity is O(n)
+// space complexity is O(1) because we are not using any extra space to store the result
+// ✅ Hints: using if condition only
