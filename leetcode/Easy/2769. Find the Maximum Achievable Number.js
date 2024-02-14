49991f3dd6a7eab36a8ccd5a632fdba8be9607d3
JoyDhar32 <joyustcian@gmail.com>
2024-02-14 12:15:29 +1100

    ✅ Hints: using if condition only

---
+++ leetcode/Easy/2769. Find the Maximum Achievable Number.js
@@ -0,0 +1,6 @@
+const theMaximumAchievableX = (num,t)=> {return (num+(t*2));};
+console.log(theMaximumAchievableX(3,2));//7
+console.log(theMaximumAchievableX(4,1));//6
+console.log(theMaximumAchievableX(3,4));//11
+console.log(theMaximumAchievableX(4,5));//14
+console.log(theMaximumAchievableX(5,6));//17
