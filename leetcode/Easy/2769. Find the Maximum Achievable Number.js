25628f3421ef27eca65531eaabff3d9ac4221f9b
JoyDhar32 <joyustcian@gmail.com>
2024-02-14 12:16:19 +1100

    ✅ Hints: multiply by 2 and add the result to the first number

--- leetcode/Easy/2769. Find the Maximum Achievable Number.js
+++ leetcode/Easy/2769. Find the Maximum Achievable Number.js
@@ -4,3 +4,7 @@ console.log(theMaximumAchievableX(4,1));//6
 console.log(theMaximumAchievableX(3,4));//11
 console.log(theMaximumAchievableX(4,5));//14
 console.log(theMaximumAchievableX(5,6));//17
+
+// time complexity is O(1)
+// space complexity is O(1)
+// ✅ Hints: multiply by 2 and add the result to the first number
