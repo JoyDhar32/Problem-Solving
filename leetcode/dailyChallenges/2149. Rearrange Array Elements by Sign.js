3e726c7b0c8b915b1436d4f0e8d4279cfb75e539
JoyDhar32 <joyustcian@gmail.com>
2024-02-14 12:06:09 +1100

    ✅ Hints: using if condition only

---
+++ leetcode/dailyChallenges/2149. Rearrange Array Elements by Sign.js
@@ -0,0 +1,18 @@
+const rearrangeArray = (nums) => {
+    let pos = [], neg = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= 0) pos.push(nums[i]);
+        else neg.push(nums[i]);
+    }
+    let res = [];
+    for (let i = 0; i < pos.length; i++) {
+        res.push(pos[i], neg[i]);
+    }
+    return res;
+}
+console.log(rearrangeArray([1, 2, 3, -1, -2, -3]));//[ [ 1, -1 ], [ 2, -2 ], [ 3, -3 ] ]
+console.log(rearrangeArray([1, 2, 3, 4, 5, 6]));//[ [ 1, -1 ], [ 2, -2 ], [ 3, -3 ] ]
+
+// time complexity is O(n)
+// space complexity is O(n) 
+// ✅ Hints: using if condition only
