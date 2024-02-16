a4b1218d844243562567ccc5958168e2e2cce1f6
JoyDhar32 <joyustcian@gmail.com>
2024-02-16 15:58:41 +1100

    ✅ Hints: using the map method and the sort method

--- leetcode/dailyChallenges/1481. Least Number of Unique Integers after K Removals.js
+++ leetcode/dailyChallenges/1481. Least Number of Unique Integers after K Removals.js
@@ -13,3 +13,7 @@ const findLeastNumOfUniqueInts = (arr, k) => {
 console.log(findLeastNumOfUniqueInts([5,5,5], 2));//1
 console.log(findLeastNumOfUniqueInts([4,3,1,1,3,3,2], 3));//2
 console.log(findLeastNumOfUniqueInts([1], 1));//0
+
+// time complexity: O(nlogn)
+// space complexity: O(n)
+// ✅ Hints: using the map method and the sort method
