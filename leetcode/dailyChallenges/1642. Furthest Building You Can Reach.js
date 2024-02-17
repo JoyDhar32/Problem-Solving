27ce7c7e439f9c70aacc844e0c8f94677f56523a
JoyDhar32 <joyustcian@gmail.com>
2024-02-17 22:30:13 +1100

    ✅ Hints: Using Priority Queue to solve the problem

--- leetcode/dailyChallenges/1642. Furthest Building You Can Reach.js
+++ leetcode/dailyChallenges/1642. Furthest Building You Can Reach.js
@@ -1,35 +1,37 @@
 
-const furthestBuilding =(heights, bricks, ladders) => {
-       
-    const heap = new PriorityQueue({compare:(a,b)=>{
-        return a-b
-    }})
+const furthestBuilding = (heights, bricks, ladders) => {
+
+    const heap = new PriorityQueue({
+        compare: (a, b) => {
+            return a - b
+        }
+    })
 
     let prevHeight = heights[0]
     let sum = 0
 
-    for(let i=1;i<heights.length;i++){
+    for (let i = 1; i < heights.length; i++) {
 
         const height = heights[i]
 
-        const diff = height-prevHeight
+        const diff = height - prevHeight
 
-        if(diff>0){
+        if (diff > 0) {
             heap.enqueue(diff)
         }
-        
-        if(heap.size()>ladders){
-            sum+=heap.dequeue()
+
+        if (heap.size() > ladders) {
+            sum += heap.dequeue()
         }
 
-        if(sum>bricks){
-            return i-1
+        if (sum > bricks) {
+            return i - 1
         }
 
         prevHeight = height
     }
 
-    return heights.length-1 
+    return heights.length - 1
 };
 console.log(furthestBuilding([4, 12, 2, 7, 3, 18, 20, 3, 19], 10, 2)); // Output: 7
 console.log(furthestBuilding([14, 3, 19, 3], 17, 0)); // Output: 3
