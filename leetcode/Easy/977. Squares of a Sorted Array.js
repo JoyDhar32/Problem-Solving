5963c4442c77fe42db758897c5dae67fa38c2a32
JoyDhar32 <joyustcian@gmail.com>
2024-02-20 11:37:56 +1100

    Basic Projects

---
+++ leetcode/Easy/977. Squares of a Sorted Array.js
@@ -0,0 +1,17 @@
+const sortedSquares = (nums)=>{
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        result.push(nums[i] ** 2);
+    }
+
+     result= result.sort((a, b) => a - b);
+    return result;
+}
+console.log(sortedSquares([-4,-1,0,3,10])); // [0,1,9,16,100]
+console.log(sortedSquares([-7,-3,2,3,11])); // [4,9,9,49,121]
+console.log(sortedSquares([1])); // [1]
+console.log(sortedSquares([0,2])); // [0,4]
+
+// Time complexity is O(nlogn)
+// Space complexity is O(n)
+// ✅ Hints: using exponentiation and sorting
