c7b328422dfd343080249bbf9759c769d3956e6f
JoyDhar32 <joyustcian@gmail.com>
2024-02-20 11:15:00 +1100

    ✅ using the sum of the first n natural numbers to find the missing number

---
+++ leetcode/dailyChallenges/268. Missing Number.js
@@ -0,0 +1,15 @@
+const missingNumber = (nums) => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return (nums.length * (nums.length + 1)) / 2 - sum;
+};
+
+console.log(missingNumber([3, 0, 1])); // 2
+console.log(missingNumber([0, 1])); // 2
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
+
+// Time complexity is O(n)
+// Space complexity is O(1)
+// ✅  using the sum of the first n natural numbers to find the missing number
