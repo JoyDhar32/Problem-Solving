f7a021654087893c246fbf133144109decef9a76
JoyDhar32 <joyustcian@gmail.com>
2024-02-23 16:44:55 +1100

    update

---
+++ leetcode/dailyChallenges/787. Cheapest Flights Within K Stops.js
@@ -0,0 +1,21 @@
+const findCheapestPrice = (n, flights, src, dst, k) => {
+    const prices = new Array(n).fill(Infinity);
+    prices[src] = 0;
+
+    for (let i = 0; i < k + 1; i++) {
+        const tmpPrices = prices.slice(); // Create a copy of prices array
+        for (const [s, d, p] of flights) {
+            if (prices[s] !== Infinity && prices[s] + p < tmpPrices[d]) {
+                tmpPrices[d] = prices[s] + p;
+            }
+        }
+        prices.splice(0, prices.length, ...tmpPrices); // Update prices array
+    }
+
+    return prices[dst] === Infinity ? -1 : prices[dst];
+};
+
+console.log(findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 1)); // 200
+console.log(findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 0)); // 500
+console.log(findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 2)); // 200
+console.log(findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 3)); // 200
