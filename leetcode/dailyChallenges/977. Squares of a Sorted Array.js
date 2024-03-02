2e0feb543d9814cf0f20f28be237128a962267bd
JoyDhar32 <joyustcian@gmail.com>
2024-03-02 14:16:55 +1100

    ✅ Hints : Use two pointers to compare the absolute values of the elements at the left and right ends of the array.

--- leetcode/dailyChallenges/977. Squares of a Sorted Array.js
+++ leetcode/dailyChallenges/977. Squares of a Sorted Array.js
@@ -17,3 +17,7 @@ const sortedSquares = (nums) => {
     console.log(sortedSquares([-4,-1,0,3,10]))//[0,1,9,16,100]
     console.log(sortedSquares([-7,-3,2,3,11]))//[4,9,9,49,121]
     console.log(sortedSquares([1]))//[1]
+
+// time complexity is O(n)
+// space complexity is O(n)
+// ✅ Hints : Use two pointers to compare the absolute values of the elements at the left and right ends of the array.
