d1780cc001417b44acc6819b6ce4f79a76ed87cd
JoyDhar32 <joyustcian@gmail.com>
2024-03-15 17:19:58 +1100

    ✅ Hint: Using two pointers

---
+++ leetcode/dailyChallenges/238. Product of Array Except Self.js
@@ -0,0 +1,17 @@
+const productExceptSelf = (nums) => {
+    let result = [];
+    let left = 1;
+    let right = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = left;
+        left *= nums[i];
+    }
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= right;
+        right *= nums[i];
+    }
+    return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4])); // [24,12,8,6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0,0,9,0,0]
