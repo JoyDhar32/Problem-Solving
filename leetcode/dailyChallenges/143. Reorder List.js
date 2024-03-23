69444408ee64697852b68f1e097d032193963b58
JoyDhar32 <joyustcian@gmail.com>
2024-03-24 00:33:12 +1100

    ✅ Hints : Use a stack to store the nodes of the linked list

--- leetcode/dailyChallenges/143. Reorder List.js
+++ leetcode/dailyChallenges/143. Reorder List.js
@@ -1,25 +1,25 @@
 var reorderList = function (head) {
-    let stack = [], node = head
-    if (!node) return
-    while (node) {
-      stack.push(node)
-      node = node.next
-    }
-    let len = stack.length
-    node = head
-    for (let i = 0; i < len; i++) {
-      if (i % 2 == 0)
-        node.next = stack.shift()
-      else
-        node.next = stack.pop()
-      node = node.next
-    }
-    node.next = null
-  };
+  let stack = [], node = head
+  if (!node) return
+  while (node) {
+    stack.push(node)
+    node = node.next
+  }
+  let len = stack.length
+  node = head
+  for (let i = 0; i < len; i++) {
+    if (i % 2 == 0)
+      node.next = stack.shift()
+    else
+      node.next = stack.pop()
+    node = node.next
+  }
+  node.next = null
+};
 
-  console.log(reorderList([1,2,3,4])); // [1,4,2,3]
-    console.log(reorderList([1,2,3,4,5])); // [1,5,2,4,3]
-    console.log(reorderList([1,2,3,4,5,6])); // [1,6,2,5,3,4]
+console.log(reorderList([1, 2, 3, 4])); // [1,4,2,3]
+console.log(reorderList([1, 2, 3, 4, 5])); // [1,5,2,4,3]
+console.log(reorderList([1, 2, 3, 4, 5, 6])); // [1,6,2,5,3,4]
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
