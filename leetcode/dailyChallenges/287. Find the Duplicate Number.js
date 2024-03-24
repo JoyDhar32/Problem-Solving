153b48c5ccb898e3c13d9252e394a15a87cba2ba
JoyDhar32 <joyustcian@gmail.com>
2024-03-24 15:15:07 +1100

    ✅ Hints : Use the Floyd's Tortoise and Hare algorithm

---
+++ leetcode/dailyChallenges/287. Find the Duplicate Number.js
@@ -0,0 +1,19 @@
+const findDuplicate = (nums) => {
+    let slow = nums[0];
+    let fast = nums[0];
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]]; 
+    } while (slow !== fast);
+    
+    slow = nums[0];
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+    };
+
+console.log(findDuplicate([1, 3, 4, 2, 2])); // 2
+console.log(findDuplicate([3, 1, 3, 4, 2])); // 3
+console.log(findDuplicate([1, 1])); // 1
