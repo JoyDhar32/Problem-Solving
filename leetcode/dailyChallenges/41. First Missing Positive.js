6708401447ef915e94dc208c6cf88573947de3e2
JoyDhar32 <joyustcian@gmail.com>
2024-03-26 11:43:55 +1100

    ✅ Hints : Use a Set to find the Duplicate Numbers

--- leetcode/dailyChallenges/41. First Missing Positive.js
+++ leetcode/dailyChallenges/41. First Missing Positive.js
@@ -1,16 +1,16 @@
 const firstMissingPositive = (nums) => {
     let set = new Set(nums);
     let i = 1;
-    while(set.has(i)) {
+    while (set.has(i)) {
         i++;
     }
     return i;
 }
 
-console.log(firstMissingPositive([1,2,0])); // 3
-console.log(firstMissingPositive([3,4,-1,1])); // 2
-console.log(firstMissingPositive([7,8,9,11,12])); // 1
-console.log(firstMissingPositive([1,1])); // 2
+console.log(firstMissingPositive([1, 2, 0])); // 3
+console.log(firstMissingPositive([3, 4, -1, 1])); // 2
+console.log(firstMissingPositive([7, 8, 9, 11, 12])); // 1
+console.log(firstMissingPositive([1, 1])); // 2
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
