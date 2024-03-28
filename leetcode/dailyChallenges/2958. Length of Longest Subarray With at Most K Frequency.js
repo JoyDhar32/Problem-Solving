410e5e288cfb65b067a2db9ff676d1acdaab6703
JoyDhar32 <joyustcian@gmail.com>
2024-03-28 11:46:51 +1100

    ✅ Hints: Sliding Window Technique

---
+++ leetcode/dailyChallenges/2958. Length of Longest Subarray With at Most K Frequency.js
@@ -0,0 +1,37 @@
+const maxSubarryLength = (nums, k) => {
+    let left = 0;
+    let maxLen = 0;
+    let exceedCount = 0;
+    const frequencyMap = new Map();
+
+    for (let right = 0; right < nums.length; right++) {
+        const currentNum = nums[right];
+        frequencyMap.set(currentNum, (frequencyMap.get(currentNum) || 0) + 1);
+
+        if (frequencyMap.get(currentNum) === k + 1) {
+            exceedCount++;
+        }
+
+        while (exceedCount > 0) {
+            const leftNum = nums[left];
+            frequencyMap.set(leftNum, frequencyMap.get(leftNum) - 1);
+            if (frequencyMap.get(leftNum) === k) {
+                exceedCount--;
+            }
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
+
+console.log(maxSubarryLength([1, 2, 1, 2, 3], 2)); // 4
+console.log(maxSubarryLength([1, 2, 1, 3, 4], 3)); // 3
+console.log(maxSubarryLength([1, 2, 1, 3, 4], 4)); // 5
+console.log(maxSubarryLength([1,2,3,1,2,3,1,2], 2)); // 5
+
+// time complexity: O(n)
+// space complexity: O(k) where k is the number of unique elements in the input array
+// ✅ Hints: Sliding Window Technique
