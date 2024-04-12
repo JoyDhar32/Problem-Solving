53cb426f2ca7b72cb7a9ba7f5cf829b81f6b6ef0
JoyDhar32 <joyustcian@gmail.com>
2024-04-13 00:15:04 +1000

    ✅ Hints : use two pointers to find the max

--- leetcode/dailyChallenges/42. Trapping Rain Water.js
+++ leetcode/dailyChallenges/42. Trapping Rain Water.js
@@ -1,21 +1,21 @@
-const trap =(height)=>{
+const trap = (height) => {
     let leftMax = 0;
     let rightMax = 0;
     let left = 0;
     let right = height.length - 1;
     let result = 0;
-    while(left < right){
-        if(height[left] < height[right]){
-            if(height[left] >= leftMax){
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
                 leftMax = height[left];
-            }else{
+            } else {
                 result += leftMax - height[left];
             }
             left++;
-        }else{
-            if(height[right] >= rightMax){
+        } else {
+            if (height[right] >= rightMax) {
                 rightMax = height[right];
-            }else{
+            } else {
                 result += rightMax - height[right];
             }
             right--;
@@ -24,10 +24,10 @@ const trap =(height)=>{
     return result;
 }
 
-console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]));//6
-console.log(trap([4,2,0,3,2,5]));//9
-console.log(trap([4,2,3]));//1
-console.log(trap([2,0,2]));//2
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));//6
+console.log(trap([4, 2, 0, 3, 2, 5]));//9
+console.log(trap([4, 2, 3]));//1
+console.log(trap([2, 0, 2]));//2
 
 // time complexity: O(n)
 // space complexity: O(1)
