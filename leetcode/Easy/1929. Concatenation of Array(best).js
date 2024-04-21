b790afb71c0c339f1b9d131d14f87ed182298830
JoyDhar32 <joyustcian@gmail.com>
2024-04-21 20:02:10 +1000

    ✅ Hints : Best Time Complexity. Array Concatenation

---
+++ leetcode/Easy/1929. Concatenation of Array(best).js
@@ -0,0 +1,19 @@
+let getConcatenation =(nums) =>{
+    let result=[];
+    len=nums.length;
+    for(let i=0;i<len;i++){
+        result.push(nums[i]);
+    }
+    for(let i=0;i<len;i++){
+        result.push(nums[i]);
+    }
+    return result;
+}
+
+console.log(getConcatenation([1,2,1])) // [1,2,1,1,2,1]
+console.log(getConcatenation([1])) // [1,1]
+console.log(getConcatenation([1,2,3])) // [1,2,3,1,2,3]
+
+// Time Complexity: O(N), where N is the number of elements in the input array.
+// Space Complexity: O(N), where N is the number of elements in the input array. We are using extra space to store the result array.
+// ✅ Hints : Best Time Complexity. Array Concatenation
