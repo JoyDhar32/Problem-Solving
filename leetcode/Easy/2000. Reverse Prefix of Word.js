a85298b7f77f157380db378f9cc7f0565b38119a
JoyDhar32 <joyustcian@gmail.com>
2024-04-21 20:07:24 +1000

    ✅ Hints: Using JS Built-in Functions

--- leetcode/Easy/2000. Reverse Prefix of Word.js
+++ leetcode/Easy/2000. Reverse Prefix of Word.js
@@ -2,10 +2,12 @@ const reversePrefix = (word, ch) => {
     let index = word.indexOf(ch);
     if (index === -1) return word;
     return word.slice(0, index + 1).split('').reverse().join('') + word.slice(index + 1);
-    };
+};
+
 console.log(reversePrefix("abcdefg", "d"));//"dcbaefg"
 console.log(reversePrefix("xyxzxe", "z"));//"zxyxxe"
 console.log(reversePrefix("abcd", "z"));//"abcd"
+
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 // ✅ Hints: Using JS Built-in Functions
