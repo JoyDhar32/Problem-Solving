f85b71253096dbcc0638d6392fc227949c5757af
JoyDhar32 <joyustcian@gmail.com>
2024-04-21 20:09:07 +1000

    ✅ Beats 100% of JavaScript submissions in runtime

--- leetcode/Easy/2185. Counting Words With a Given Prefix.js
+++ leetcode/Easy/2185. Counting Words With a Given Prefix.js
@@ -1,13 +1,14 @@
-const prefixCount = (words,pref) =>{
+const prefixCount = (words, pref) => {
     let count = 0;
-    for(let i=0;i<words.length;i++){
-        if(words[i].startsWith(pref)){
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].startsWith(pref)) {
             count++;
         }
     }
     return count;
 }
 
+console.log(prefixCount(["ab", "abc", "def", "ghi"], "ab"));
+console.log(prefixCount(["ab", "abc", "def", "ghi"], "de"));
+
 // ✅ Beats 100% of JavaScript submissions in runtime
- console.log(prefixCount(["ab","abc","def","ghi"],"ab"));
- console.log(prefixCount(["ab","abc","def","ghi"],"de"));
