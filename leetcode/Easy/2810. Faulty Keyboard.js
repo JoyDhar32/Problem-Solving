4dcd5e04a269dacb6edc6d0b30355a71933ae5f3
JoyDhar32 <joyustcian@gmail.com>
2024-04-28 22:10:50 +1000

    ✅ Hints: Use a stack to reverse the string when you encounter the letter

--- leetcode/Easy/2810. Faulty Keyboard.js
+++ leetcode/Easy/2810. Faulty Keyboard.js
@@ -19,3 +19,7 @@ console.log(finalString("string"));
 console.log(finalString("poiinter"));
 
 //✅ Beat 55% of solutions on leetcode
+//✅ Time complexity is O(n)
+//✅ Space complexity is O(n)
+
+//✅ Hints: Use a stack to reverse the string when you encounter the letter 'i'. Otherwise, append the letter to the string.
