b4f206663bd615fbef35dddc3959d439c80d8c41
JoyDhar32 <joyustcian@gmail.com>
2024-04-28 22:12:20 +1000

    ✅ Hints: Use a set to store the reversed strings and count the number of pairs.

--- leetcode/Easy/2744. Find Maximum Number of String Pairs.js
+++ leetcode/Easy/2744. Find Maximum Number of String Pairs.js
@@ -1,4 +1,4 @@
-const maximumNumberOfStringParis =(words) =>{
+const maximumNumberOfStringParis = (words) => {
     let count = 0;
     const reversedSet = new Set();
 
@@ -15,10 +15,11 @@ const maximumNumberOfStringParis =(words) =>{
 
     return count;
 }
-console.log(maximumNumberOfStringParis(["cd","ac","dc","ca","zz"])); // 2
-console.log(maximumNumberOfStringParis(["ab","ba","cc"])); // 1
+console.log(maximumNumberOfStringParis(["cd", "ac", "dc", "ca", "zz"])); // 2
+console.log(maximumNumberOfStringParis(["ab", "ba", "cc"])); // 1
 
 // time complexity: O(N * K)
 // space complexity: O(N * K)
 
 // ✅ Beats 100% of users with JavaScript
+// ✅ Hints: Use a set to store the reversed strings and count the number of pairs.
