28a4d230200b7abd5495bd79a02fc3fd747dabd2
JoyDhar32 <joyustcian@gmail.com>
2024-04-28 22:11:47 +1000

    ✅ Hints: multiply by 2 and add the result to the first number

--- leetcode/Easy/2769. Find the Maximum Achievable Number.js
+++ leetcode/Easy/2769. Find the Maximum Achievable Number.js
@@ -1,9 +1,9 @@
-const theMaximumAchievableX = (num,t)=> {return (num+(t*2));};
-console.log(theMaximumAchievableX(3,2));//7
-console.log(theMaximumAchievableX(4,1));//6
-console.log(theMaximumAchievableX(3,4));//11
-console.log(theMaximumAchievableX(4,5));//14
-console.log(theMaximumAchievableX(5,6));//17
+const theMaximumAchievableX = (num, t) => { return (num + (t * 2)); };
+console.log(theMaximumAchievableX(3, 2));//7
+console.log(theMaximumAchievableX(4, 1));//6
+console.log(theMaximumAchievableX(3, 4));//11
+console.log(theMaximumAchievableX(4, 5));//14
+console.log(theMaximumAchievableX(5, 6));//17
 
 // time complexity is O(1)
 // space complexity is O(1)
