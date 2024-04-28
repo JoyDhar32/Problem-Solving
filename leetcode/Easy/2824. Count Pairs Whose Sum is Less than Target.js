b0c99a1622293d636357f1876789478b2ab3c486
JoyDhar32 <joyustcian@gmail.com>
2024-04-28 22:10:11 +1000

    ✅ Hints: Use a nested loop to iterate over

--- leetcode/Easy/2824. Count Pairs Whose Sum is Less than Target.js
+++ leetcode/Easy/2824. Count Pairs Whose Sum is Less than Target.js
@@ -1,16 +1,16 @@
-const countPairs =(nums, target) =>{
+const countPairs = (nums, target) => {
     let count = 0;
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]<target){
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
                 count++;
             }
         }
     }
     return count;
 }
-console.log(countPairs([-1,1,2,3,1], 2)); // Output: 3
-console.log(countPairs([-6,2,5,-2,-7,-1,3], -2)); // Output: 4
+console.log(countPairs([-1, 1, 2, 3, 1], 2)); // Output: 3
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2)); // Output: 4
 console.log(countPairs([1, 2], 2)); // Output: 0
 console.log(countPairs([1, 2, 3], 3)); // Output: 1
 
@@ -18,3 +18,4 @@ console.log(countPairs([1, 2, 3], 3)); // Output: 1
 // Space Complexity: O(1)
 
 //  ✅ Beats 100% of JavaScript Submissions
+// ✅ Hints: Use a nested loop to iterate over all pairs of elements and count the pairs whose sum is less than the target.
