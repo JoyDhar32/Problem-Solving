0f16852834fd5edc44fdeeff8a589f67febbaf2f
JoyDhar32 <joyustcian@gmail.com>
2024-05-03 12:38:23 +1000

    update

---
+++ leetcode/medium/128. Longest Consecutive Sequence.js
@@ -0,0 +1,28 @@
+const longestConsecutive = (nums) => {
+    const set = new Set(nums);
+    let longest = 0;
+    
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+        let currentNum = num;
+        let currentLength = 1;
+    
+        while (set.has(currentNum + 1)) {
+            currentNum++;
+            currentLength++;
+        }
+    
+        longest = Math.max(longest, currentLength);
+        }
+    }
+    
+    return longest;
+    };
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // 4
+ console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1])); // 9
+ console.log(longestConsecutive([1,2,0,1])); // 3
+
+    // time complexity: O(n)
+    // space complexity: O(n)
+    // 
