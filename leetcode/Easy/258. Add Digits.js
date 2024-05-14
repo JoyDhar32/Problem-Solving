593a7bf1a467f47a370ee440ec6ebeaf725386f0
JoyDhar32 <joyustcian@gmail.com>
2024-05-14 11:49:57 +1000

    ✅ Hints: using if condition only

---
+++ leetcode/Easy/258. Add Digits.js
@@ -0,0 +1,12 @@
+const addDigits =(num)=>{
+    if(num==0) return 0;
+   return num%9==0?9:num%9;
+}
+
+console.log(addDigits(38));//2
+console.log(addDigits(0));//0
+console.log(addDigits(9));//9
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+//✅ Hints: using if condition only
