e36015e8bdb0b98960e82d79c194b25c7f702d01
JoyDhar32 <joyustcian@gmail.com>
2024-05-16 19:04:42 +1000

    ✅ Hints: use the sort method to sort the array in ascending order

---
+++ leetcode/Easy/908. Smallest Range I.js
@@ -0,0 +1,15 @@
+const smallestRangeI = (nums, k) => {
+    nums.sort((a,b)=>a-b);
+    let min=nums[0];
+    let max=nums[nums.length-1];
+    if((max-k)<=(min+k)) return 0;
+    else return ((max-k)-(min+k));
+    };
+
+console.log(smallestRangeI([1], 0));//0
+console.log(smallestRangeI([0,10], 2));//6
+console.log(smallestRangeI([1,3,6], 3));//0
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1) because we are not using any extra space to store the result
+//✅ Hints: use the sort method to sort the array in ascending order
