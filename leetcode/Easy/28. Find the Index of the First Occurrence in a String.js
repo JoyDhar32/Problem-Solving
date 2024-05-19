2f79b920b922c095f2ec0ec7fc1d7843f53c08fc
JoyDhar32 <joyustcian@gmail.com>
2024-05-19 13:03:24 +1000

    ✅ Hints: use the sort method to sort the array in ascending order

--- leetcode/Easy/28. Find the Index of the First Occurrence in a String.js
+++ leetcode/Easy/28. Find the Index of the First Occurrence in a String.js
@@ -1,15 +1,15 @@
 const strStr = (haystack, needle) => {
     if (needle === '') return 0;
     if (haystack === '') return -1;
-    
+
     for (let i = 0; i < haystack.length; i++) {
         if (haystack[i] === needle[0]) {
-        if (haystack.slice(i, i + needle.length) === needle) return i;
+            if (haystack.slice(i, i + needle.length) === needle) return i;
         }
     }
-    
+
     return -1;
-    }
+}
 
 console.log(strStr('hello', 'll'));//2
 console.log(strStr('aaaaa', 'bba'));//-1
@@ -17,4 +17,4 @@ console.log(strStr('', ''));//0
 
 // Time Complexity: O(n)
 // Space Complexity: O(1) because we are not using any extra space to store the result
-//✅ Hints: use the sort method to sort the array in ascending order
+// ✅ Hints: use the sort method to sort the array in ascending order
