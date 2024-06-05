3e76596a6a5a61873fb4b60ccc1c572de35a9a80
JoyDhar32 <joyustcian@gmail.com>
2024-06-05 16:59:15 +1000

    ✅ Hints: where n is the number of promises

--- leetcode/Easy/2723. Add Two Promises.js
+++ leetcode/Easy/2723. Add Two Promises.js
@@ -1,7 +1,7 @@
 const addTwoPromises = async (promise1, promise2) => {
     const [val1, val2] = await Promise.all([promise1, promise2]);
     return val1 + val2;
-    };
+};
 
 console.log(addTwoPromises(Promise.resolve(3), Promise.resolve(5))); // 8
 console.log(addTwoPromises(Promise.resolve(7), Promise.resolve(2))); // 9
