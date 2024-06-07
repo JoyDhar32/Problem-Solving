7fa3cecc49c00a157d2b82b0d47180c3a21f5e49
JoyDhar32 <joyustcian@gmail.com>
2024-06-08 00:09:49 +1000

    ✅ Hints: use Set to store unique values and compare the length of the array and the size of the set

--- leetcode/Easy/217. Contains Duplicate.js
+++ leetcode/Easy/217. Contains Duplicate.js
@@ -1,12 +1,12 @@
-const containsDuplicate=(nums)=>{
+const containsDuplicate = (nums) => {
     let set = new Set(nums);
-    let beforeLen=nums.length;
-    let afterLen=set.size;
-    return (beforeLen !=afterLen);
+    let beforeLen = nums.length;
+    let afterLen = set.size;
+    return (beforeLen != afterLen);
 };
-console.log(containsDuplicate([1,2,3,1])); // true
-console.log(containsDuplicate([1,2,3,4])); // false
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // true
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
 
 // time complexity: O(n)
 // space complexity: O(n)
