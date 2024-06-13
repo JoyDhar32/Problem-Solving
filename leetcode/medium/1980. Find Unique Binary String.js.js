b85ae426cc30d2a37a01cfdc6ac54aff796b4214
JoyDhar32 <joyustcian@gmail.com>
2024-06-13 11:31:40 +1000

    ✅ Hints: use a set to store the unique binary strings and iterate through all the possible binary strings

---
+++ leetcode/medium/1980. Find Unique Binary String.js.js
@@ -0,0 +1,12 @@
+const findDifferentBinaryString = (nums) => {
+  const n = nums.length;
+  const set = new Set(nums);
+  for (let i = 0; i < 1 << n; i++) {
+    const str = i.toString(2).padStart(n, '0'); 
+    if (!set.has(str)) return str;
+  }
+};
+
+// time complexity: O(2^n)
+// space complexity: O(2^n)
+// ✅ Hints: use a set to store the unique binary strings and iterate through all the possible binary strings
