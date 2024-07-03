c37f5c60801d5c603cc3e7e0cd899e95d3aa486a
JoyDhar32 <joyustcian@gmail.com>
2024-07-03 13:49:17 +1000

    ✅ Hints: Sort the array and calculate the difference between the largest and smallest elements

--- leetcode/dailyChallenges/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.js
+++ leetcode/dailyChallenges/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.js
@@ -13,3 +13,17 @@ const minDifference = (nums) => {
 console.log(minDifference([1, 2, 2, 1])) // 0
 console.log(minDifference([4, 9, 5])) // 1
 console.log(minDifference([1, 5, 0, 10, 14])) // 8
+
+// time complexity: O(nlogn) /* due to sorting     * /
+// space complexity: O(1)
+// ✅ Hints: Sort the array and calculate the difference between the largest and smallest elements. There are four possible ways to remove three elements from the array to minimize the difference. Return the minimum difference.
+
+/* Explanation: 
+Sorting the array: [0, 1, 1, 4, 6, 6, 6]
+Four scenarios to consider:
+Change the three smallest values: nums[length-1] - nums[3] = 6 - 4 = 2
+Change two smallest and the largest value: nums[length-2] - nums[2] = 6 - 1 = 5
+Change the smallest and two largest values: nums[length-3] - nums[1] = 6 - 1 = 5
+Change the three largest values: nums[length-4] - nums[0] = 6 - 0 = 6
+From these, the minimum difference is 2.
+*/
