be5dfafd34f2c0e5ac235fa22923c0a828dbe9b3
JoyDhar32 <joyustcian@gmail.com>
2024-07-08 21:55:54 +1000

    ✅ Hint: Loop through the array and apply the function to each element

--- leetcode/Easy/2634. Filter Elements from Array.js
+++ leetcode/Easy/2634. Filter Elements from Array.js
@@ -1,9 +1,9 @@
-const filter =(arr, fn)=>{
-    let length =arr.length;
-    let filteredArray =[];
-    let i=0;
-    while(i<length){
-        if(fn(arr[i],i)){
+const filter = (arr, fn) => {
+    let length = arr.length;
+    let filteredArray = [];
+    let i = 0;
+    while (i < length) {
+        if (fn(arr[i], i)) {
             filteredArray.push(arr[i]);
         }
         i++;
@@ -12,10 +12,10 @@ const filter =(arr, fn)=>{
 }
 
 // Test cases
-console.log(filter([1,2,3],(x)=>x>1));//[2,3]
-console.log(filter([1,2,3],(x)=>x<2));//[1]
-console.log(filter([1,2,3],(x)=>x%2===0));//[2]
-console.log(filter([1,2,3],(x)=>x%2!==0));//[1,3]
+console.log(filter([1, 2, 3], (x) => x > 1));//[2,3]
+console.log(filter([1, 2, 3], (x) => x < 2));//[1]
+console.log(filter([1, 2, 3], (x) => x % 2 === 0));//[2]
+console.log(filter([1, 2, 3], (x) => x % 2 !== 0));//[1,3]
 
 // Time complexity: O(n)
 // Space complexity: O(n)
