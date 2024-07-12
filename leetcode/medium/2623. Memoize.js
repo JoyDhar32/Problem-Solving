8c0d5722d255578eaa34318ce5747f6811b7d1c4
JoyDhar32 <joyustcian@gmail.com>
2024-07-12 12:27:39 +1000

    ✅ Hint: Use an object to store the results of the function calls

--- leetcode/medium/2623. Memoize.js
+++ leetcode/medium/2623. Memoize.js
@@ -1,17 +1,17 @@
 const memoize = (fn) => {
-let output ={};
-return function(...args){
-    if(args in output){
-        return output[args];
-    }else{
-        output[args] = fn(...args);
-        return output[args];
+    let output = {};
+    return function (...args) {
+        if (args in output) {
+            return output[args];
+        } else {
+            output[args] = fn(...args);
+            return output[args];
+        }
     }
 }
-    }
-    const add = (a, b) => a + b;
-    const memoizedAdd = memoize(add);
+const add = (a, b) => a + b;
+const memoizedAdd = memoize(add);
 
-    // Time Complexity: O(1)
-    // Space Complexity: O(n)
-    // ✅ Hint: Use an object to store the results of the function calls
+// Time Complexity: O(1)
+// Space Complexity: O(n)
+// ✅ Hint: Use an object to store the results of the function calls
