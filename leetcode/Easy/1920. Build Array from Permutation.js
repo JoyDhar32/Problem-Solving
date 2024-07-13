fa27e5aaafbaa9670bd16d1f453ce09cae9ca021
JoyDhar32 <joyustcian@gmail.com>
2024-07-13 23:25:59 +1000

    ✅ Hints: Create a new array and store

--- leetcode/Easy/1920. Build Array from Permutation.js
+++ leetcode/Easy/1920. Build Array from Permutation.js
@@ -1,13 +1,13 @@
 const buildArray = (nums) => {
     let result = [];
-    for(let i=0; i<nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         result.push(nums[nums[i]]);
     }
     return result;
 }
 
-console.log(buildArray([0,2,1,5,3,4])) // [0,1,2,4,5,3]
-console.log(buildArray([5,0,1,2,3,4])) // [4,5,0,1,2,3]
+console.log(buildArray([0, 2, 1, 5, 3, 4])) // [0,1,2,4,5,3]
+console.log(buildArray([5, 0, 1, 2, 3, 4])) // [4,5,0,1,2,3]
 
 // time complexity: O(n)
 // space complexity: O(n) /* to store the result array */
