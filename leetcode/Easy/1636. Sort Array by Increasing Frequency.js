d17d72fe3889e27b4f8bcafaee7785517f0ea62a
JoyDhar32 <joyustcian@gmail.com>
2024-07-23 18:19:32 +1000

    ✅ Hints: Sorting, Hash Map

--- leetcode/Easy/1636. Sort Array by Increasing Frequency.js
+++ leetcode/Easy/1636. Sort Array by Increasing Frequency.js
@@ -1,15 +1,15 @@
 const frequencySort = (nums) => {
-    let map = new Map();
-    for (let num of nums) {
-        map.set(num, map.get(num) + 1 || 1);
+    let map = new Map(); // Create a map to store the frequency of each number in the array 
+    for (let num of nums) { // Iterate through the array and update the frequency of each number in the map 
+        map.set(num, map.get(num) + 1 || 1); // If the number is not in the map, set the frequency to 1 
     }
-    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a);
+    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a); // Sort the array based on the frequency of each number. If two numbers have the same frequency, sort them in descending order 
 }
 
 console.log(frequencySort([1, 1, 2, 2, 2, 3])); // [3,1,1,2,2,2]
 console.log(frequencySort([2, 3, 1, 3, 2])); // [1,3,3,2,2]
 console.log(frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1])); // [5,-1,4,4,-6,-6,1,1,1]
 
-// time complexity: O(nlogn)
+// time complexity: O(nlogn) - sorting the array 
 // space complexity: O(n)
 // ✅ Hints: Sorting, Hash Map
