77bcccf3a15c955e88ecb3b7fe09f72ab9a47c9c
JoyDhar32 <joyustcian@gmail.com>
2024-07-26 14:40:10 +1000

    ✅ Hints: Greedy Algorithm

--- leetcode/medium/55. Jump Game.js
+++ leetcode/medium/55. Jump Game.js
@@ -15,3 +15,23 @@ console.log(canJump([0])); // true
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 // ✅ Hints: Greedy Algorithm
+
+/*
+The Problem is to determine if you are able to reach the last index of the array.
+The idea is to iterate through the array and update the maximum index that can be reached. 
+If the current index is greater than the maximum index, it means we can't reach the end of the array.
+
+Example:
+[2, 3, 1, 1, 4] => true 
+
+Index 0: 
+The first element is 2, so we can jump to the second element.
+max = Math.max(0, 0 + 2) = 2
+We can reach up to index 2 from here.
+
+
+Index 1:
+max = Math.max(2, 1 + 3) = 4
+We can reach up to index 4 from here.
+
+*/
