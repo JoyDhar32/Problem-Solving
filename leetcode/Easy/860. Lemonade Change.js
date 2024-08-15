7f93b628ae17c85ec0f70599a520fea883ef916f
JoyDhar32 <joyustcian@gmail.com>
2024-08-15 12:04:59 +1000

    ✅ Hint: Greedy Algorithm

---
+++ leetcode/Easy/860. Lemonade Change.js
@@ -0,0 +1,35 @@
+const lemonadeChange = (bills) => {
+    let five = 0;
+    let ten = 0;
+    for (let i = 0; i < bills.length; i++) {
+        if (bills[i] === 5) {
+            five++;
+        } else if (bills[i] === 10) {
+            if (five === 0) {
+                return false;
+            }
+            five--;
+            ten++;
+        } else {
+            if (five > 0 && ten > 0) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five -= 3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// test cases
+console.log(lemonadeChange([5, 5, 5, 10, 20])); // true
+console.log(lemonadeChange([5, 5, 10])); // true
+console.log(lemonadeChange([10, 10])); // false
+console.log(lemonadeChange([5, 5, 10, 10, 20])); // false
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// ✅ Hint: Greedy Algorithm
