228e40bb081dca21f70ae5920d6d25086e8fd8e5
JoyDhar32 <joyustcian@gmail.com>
2024-08-16 12:18:00 +1000

    ✅ Hint: Greedy Algorithm

---
+++ leetcode/medium/624. Maximum Distance in Arrays.js
@@ -0,0 +1,22 @@
+const maxDistance = (arrays) => {
+    let res = 0;
+    let min = arrays[0][0];
+    let max = arrays[0][arrays[0].length - 1]; // max represents the maximum value of the first array in arrays array
+    // return max;
+     let arraysLen = arrays.length;
+    // return arraysLen;
+    for (let i = 1; i <arraysLen; i++) {
+        res = Math.max(res, Math.abs(arrays[i][arrays[i].length - 1] - min), Math.abs(max - arrays[i][0]));
+        min = Math.min(min, arrays[i][0]);
+        max = Math.max(max, arrays[i][arrays[i].length - 1]);
+    }
+    return res;
+}
+// test cases
+console.log(maxDistance([[1, 2, 3], [4, 5], [1, 2, 3]])); // 4
+console.log(maxDistance([[1], [1, 2]])); // 1
+console.log(maxDistance([[1], [2]])); // 1
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// ✅ Hint: Greedy Algorithm 
