e74a1ff16010c551ed7db8a0f7641c83f68263e9
JoyDhar32 <joyustcian@gmail.com>
2024-08-17 21:11:13 +1000

    ✅ Hint: Greedy Algorithm

--- leetcode/medium/624. Maximum Distance in Arrays.js
+++ leetcode/medium/624. Maximum Distance in Arrays.js
@@ -3,9 +3,9 @@ const maxDistance = (arrays) => {
     let min = arrays[0][0];
     let max = arrays[0][arrays[0].length - 1]; // max represents the maximum value of the first array in arrays array
     // return max;
-     let arraysLen = arrays.length;
+    let arraysLen = arrays.length;
     // return arraysLen;
-    for (let i = 1; i <arraysLen; i++) {
+    for (let i = 1; i < arraysLen; i++) {
         res = Math.max(res, Math.abs(arrays[i][arrays[i].length - 1] - min), Math.abs(max - arrays[i][0]));
         min = Math.min(min, arrays[i][0]);
         max = Math.max(max, arrays[i][arrays[i].length - 1]);
