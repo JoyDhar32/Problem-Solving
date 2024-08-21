052053835b6476462eaee1ef768591076593e127
JoyDhar32 <joyustcian@gmail.com>
2024-08-21 13:58:57 +1000

    ✅ Hints: Using Stack | 88 ms Beats

--- leetcode/medium/735. Asteroid Collision.js
+++ leetcode/medium/735. Asteroid Collision.js
@@ -1,20 +1,24 @@
 const asteroidCollision = (asteroids) => {
-    let output=[];
-    for(let asteroid of asteroids){
-let len=output.length;
-let pre=output[len-1];
-if(pre<0 || asteroid>0 || !len) output.push(asteroid)
-else if(-asteroid== pre) output.pop();
-else if(-asteroid>pre) {
-    output.pop();
-    asteroids.unshift(asteroid);
-}
-}
-return output;
+    let output = [];
+    for (let asteroid of asteroids) {
+        let len = output.length;
+        let pre = output[len - 1];
+        if (pre < 0 || asteroid > 0 || !len) output.push(asteroid)
+        else if (-asteroid == pre) output.pop();
+        else if (-asteroid > pre) {
+            output.pop();
+            asteroids.unshift(asteroid);
+        }
+    }
+    return output;
 
-    };
+};
 
 console.log(asteroidCollision([5, 10, -5])) // [5, 10]
 console.log(asteroidCollision([8, -8])) // []
 console.log(asteroidCollision([10, 2, -5])) // [10]
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// ✅ Hints: Using Stack | 88 ms Beats
 
