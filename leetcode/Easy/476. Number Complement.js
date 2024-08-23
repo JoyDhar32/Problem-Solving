ad92b0a4c30e8a61180efacfef297a035c0feea3
JoyDhar32 <joyustcian@gmail.com>
2024-08-23 23:34:33 +1000

    ✅ Hints: Use XOR operation with mask |Beats 100% JS submissions

--- leetcode/Easy/476. Number Complement.js
+++ leetcode/Easy/476. Number Complement.js
@@ -1,7 +1,7 @@
-const findComplement = (num)=>{
-    let mask=1;
-    while(mask<num) mask=(mask<<1)+1;
-    return num^mask;
+const findComplement = (num) => {
+    let mask = 1;
+    while (mask < num) mask = (mask << 1) + 1;
+    return num ^ mask;
 }
 
 console.log(findComplement(5)) // 2
@@ -9,5 +9,5 @@ console.log(findComplement(1)) // 0
 console.log(findComplement(10)) // 5
 
 // Time complexity: O(1)
-// Space complexity: O(1)   
+// Space complexity: O(1)
 // ✅ Hints: Use XOR operation with mask |Beats 100% JS submissions 
