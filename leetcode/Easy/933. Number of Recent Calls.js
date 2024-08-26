7a04b6ac68e278e97c1f837ec995452bb2fd0484
JoyDhar32 <joyustcian@gmail.com>
2024-08-26 12:11:50 +1000

    ✅ Hints: Use queue to store the requests and remove the requests that are older than 3000 ms |Beats 82% JS submissions

--- leetcode/Easy/933. Number of Recent Calls.js
+++ leetcode/Easy/933. Number of Recent Calls.js
@@ -1,8 +1,8 @@
-var RecentCounter = function() {
+var RecentCounter = function () {
     this.requests = [];
 }
 
-RecentCounter.prototype.ping = function(t) {
+RecentCounter.prototype.ping = function (t) {
     this.requests.push(t);
     while (this.requests[0] < t - 3000) {
         this.requests.shift();
