c1a57e3ca625045f14342e87d609381442e33717
JoyDhar32 <joyustcian@gmail.com>
2024-09-21 22:21:31 +1000

    ✅ Hints: just loop through the array and subtract the current number from the total number of coins until the total number of coins is less than the current number. Then return the current number minus 1.

--- leetcode/Easy/441. Arranging Coins.js
+++ leetcode/Easy/441. Arranging Coins.js
@@ -7,10 +7,12 @@ const arrangeCoins = (n) => {
     return i - 1;
 }
 
+
 console.log(arrangeCoins(5)); // 2
 console.log(arrangeCoins(8)); // 3
 console.log(arrangeCoins(10)); // 4.
 
+
 // Time Complexity: O(n)
 // Space Complexity: O(1)
-// ✅ Hints: just loop through the array and subtract the current number from the total number of coins until the total number of coins is less than the current number. Then return the current number minus 1.
+// ✅ Hints: just loop through the array and subtract the current number from the total number of coins until the total number of coins is less than the current number. Then return the current number minus 1. 
